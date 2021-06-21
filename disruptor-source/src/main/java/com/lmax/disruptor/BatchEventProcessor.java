db11645459437a8f2d84964d55486bbd64b03724
wangxiang <1092697921@qq.com>
2021-06-22 07:42:01 +0800

    上传disruptor源码分析

---
+++ disruptor-source/src/main/java/com/lmax/disruptor/BatchEventProcessor.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2011 LMAX Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lmax.disruptor;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/**
+ * Convenience class for handling the batching semantics of consuming entries from a {@link RingBuffer}
+ * and delegating the available events to an {@link EventHandler}.
+ * <p>
+ * If the {@link EventHandler} also implements {@link LifecycleAware} it will be notified just after the thread
+ * is started and just before the thread is shutdown.
+ *
+ * @param <T> event implementation storing the data for sharing during exchange or parallel coordination of an event.
+ */
+public final class BatchEventProcessor<T>
+        implements EventProcessor {
+    private static final int IDLE = 0;
+    private static final int HALTED = IDLE + 1;
+    private static final int RUNNING = HALTED + 1;
+
+    private final AtomicInteger running = new AtomicInteger(IDLE);
+    private ExceptionHandler<? super T> exceptionHandler = new FatalExceptionHandler();
+    private final DataProvider<T> dataProvider;
+    private final SequenceBarrier sequenceBarrier;
+    private final EventHandler<? super T> eventHandler;
+    private final Sequence sequence = new Sequence(Sequencer.INITIAL_CURSOR_VALUE);
+    private final TimeoutHandler timeoutHandler;
+    private final BatchStartAware batchStartAware;
+
+    /**
+     * Construct a {@link EventProcessor} that will automatically track the progress by updating its sequence when
+     * the {@link EventHandler#onEvent(Object, long, boolean)} method returns.
+     *
+     * @param dataProvider    to which events are published.
+     * @param sequenceBarrier on which it is waiting.
+     * @param eventHandler    is the delegate to which events are dispatched.
+     */
+    public BatchEventProcessor(
+            final DataProvider<T> dataProvider,
+            final SequenceBarrier sequenceBarrier,
+            final EventHandler<? super T> eventHandler) {
+        this.dataProvider = dataProvider;
+        this.sequenceBarrier = sequenceBarrier;
+        this.eventHandler = eventHandler;
+
+        if (eventHandler instanceof SequenceReportingEventHandler) {
+            ((SequenceReportingEventHandler<?>) eventHandler).setSequenceCallback(sequence);
+        }
+
+        batchStartAware =
+                (eventHandler instanceof BatchStartAware) ? (BatchStartAware) eventHandler : null;
+        timeoutHandler =
+                (eventHandler instanceof TimeoutHandler) ? (TimeoutHandler) eventHandler : null;
+    }
+
+    @Override
+    public Sequence getSequence() {
+        return sequence;
+    }
+
+    @Override
+    public void halt() {
+        running.set(HALTED);
+        sequenceBarrier.alert();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running.get() != IDLE;
+    }
+
+    /**
+     * Set a new {@link ExceptionHandler} for handling exceptions propagated out of the {@link BatchEventProcessor}
+     *
+     * @param exceptionHandler to replace the existing exceptionHandler.
+     */
+    public void setExceptionHandler(final ExceptionHandler<? super T> exceptionHandler) {
+        if (null == exceptionHandler) {
+            throw new NullPointerException();
+        }
+
+        this.exceptionHandler = exceptionHandler;
+    }
+
+    /**
+     * It is ok to have another thread rerun this method after a halt().
+     *
+     * @throws IllegalStateException if this object instance is already running in a thread
+     */
+    @Override
+    public void run() {
+        if (running.compareAndSet(IDLE, RUNNING)) {
+            sequenceBarrier.clearAlert();
+
+            notifyStart();
+            try {
+                if (running.get() == RUNNING) {
+                    processEvents();
+                }
+            } finally {
+                notifyShutdown();
+                running.set(IDLE);
+            }
+        } else {
+            // This is a little bit of guess work.  The running state could of changed to HALTED by
+            // this point.  However, Java does not have compareAndExchange which is the only way
+            // to get it exactly correct.
+            if (running.get() == RUNNING) {
+                throw new IllegalStateException("Thread is already running");
+            } else {
+                earlyExit();
+            }
+        }
+    }
+
+    private void processEvents() {
+
+        T event = null;
+        // 获取要消费的下一个序号，
+        // 单线程中的sequence 仅仅是初始化一下最开始的序号。一个线程只会执行一个eventHandler
+        long nextSequence = sequence.get() + 1L;
+
+        while (true) {
+            try {
+                // 序列栅栏获取当前可用于消费到的序号
+                final long availableSequence = sequenceBarrier.waitFor(nextSequence);
+                if (batchStartAware != null && availableSequence >= nextSequence) {
+                    batchStartAware.onBatchStart(availableSequence - nextSequence + 1);
+                }
+
+                // 如果下一个要消费的序号 小于等于 可用的序号，则直接轮训其之间所有的序号，消费其中的事件
+                while (nextSequence <= availableSequence) {
+                    // dataProvider 的get实现在ringbuffer中，所以这句是获取ringbuffer中的数据
+                    event = dataProvider.get(nextSequence);
+                    // 消费事件
+                    eventHandler.onEvent(event, nextSequence, nextSequence == availableSequence);
+                    nextSequence++;
+                }
+
+                // 上面消费到了可用序号后，将可用序号设置到sequence，供下一轮使用
+                sequence.set(availableSequence);
+            } catch (final TimeoutException e) {
+                notifyTimeout(sequence.get());
+            } catch (final AlertException ex) {
+                if (running.get() != RUNNING) {
+                    break;
+                }
+            } catch (final Throwable ex) {
+                exceptionHandler.handleEventException(ex, nextSequence, event);
+                sequence.set(nextSequence);
+                nextSequence++;
+            }
+        }
+    }
+
+    private void earlyExit() {
+        notifyStart();
+        notifyShutdown();
+    }
+
+    private void notifyTimeout(final long availableSequence) {
+        try {
+            if (timeoutHandler != null) {
+                timeoutHandler.onTimeout(availableSequence);
+            }
+        } catch (Throwable e) {
+            exceptionHandler.handleEventException(e, availableSequence, null);
+        }
+    }
+
+    /**
+     * Notifies the EventHandler when this processor is starting up
+     */
+    private void notifyStart() {
+        if (eventHandler instanceof LifecycleAware) {
+            try {
+                ((LifecycleAware) eventHandler).onStart();
+            } catch (final Throwable ex) {
+                exceptionHandler.handleOnStartException(ex);
+            }
+        }
+    }
+
+    /**
+     * Notifies the EventHandler immediately prior to this processor shutting down
+     */
+    private void notifyShutdown() {
+        if (eventHandler instanceof LifecycleAware) {
+            try {
+                ((LifecycleAware) eventHandler).onShutdown();
+            } catch (final Throwable ex) {
+                exceptionHandler.handleOnShutdownException(ex);
+            }
+        }
+    }
+}
---
+++ disruptor-source/src/main/java/com/lmax/disruptor/WaitStrategy.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011 LMAX Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lmax.disruptor;
+
+
+/**
+ * Strategy employed for making {@link EventProcessor}s wait on a cursor {@link Sequence}.
+ */
+public interface WaitStrategy
+{
+    /**
+     * Wait for the given sequence to be available.  It is possible for this method to return a value
+     * less than the sequence number supplied depending on the implementation of the WaitStrategy.  A common
+     * use for this is to signal a timeout.  Any EventProcessor that is using a WaitStrategy to get notifications
+     * about message becoming available should remember to handle this case.  The {@link BatchEventProcessor} explicitly
+     * handles this case and will signal a timeout if required.
+     *
+     * 等待给予的序列号可以用。提供依赖了WaitStrategy类的实现了这个方法的，有可能会返回一个比sequence小的值。
+     * 通用的使用是发出超时信号。
+     * 任何使用WaitStrategy的EventProcessor，当或缺的消息变成可用的通知后，都应该去处理这种情况。
+     * dependentSequence 是当前event依赖的上一个事件的序号
+     *
+     *
+     * @param sequence          to be waited on.
+     * @param cursor            the main sequence from ringbuffer. Wait/notify strategies will
+     *                          need this as it's the only sequence that is also notified upon update.
+     * @param dependentSequence on which to wait.
+     * @param barrier           the processor is waiting on.
+     * @return the sequence that is available which may be greater than the requested sequence.
+     * @throws AlertException       if the status of the Disruptor has changed.
+     * @throws InterruptedException if the thread is interrupted.
+     * @throws TimeoutException if a timeout occurs before waiting completes (not used by some strategies)
+     */
+    long waitFor(long sequence, Sequence cursor, Sequence dependentSequence, SequenceBarrier barrier)
+        throws AlertException, InterruptedException, TimeoutException;
+
+    /**
+     * Implementations should signal the waiting {@link EventProcessor}s that the cursor has advanced.
+     */
+    void signalAllWhenBlocking();
+}
